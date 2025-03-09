6a69b153a0572b92255331b44d039e84ad7f1372
ThietNLU <23130314@st.hcmuaf.edu.vn>
2025-03-09 19:36:40 +0700

    Update library management system package

---
+++ src/libraryManagementSystem/Book.java
@@ -0,0 +1,14 @@
+package libraryManagementSystem;
+
+public class Book {
+	private String bookId;
+	private String title;
+	private String author;
+	private String category;
+	private int publishYear;
+	private int totalBook;
+	private int availableBook;
+	
+	
+	
+}
